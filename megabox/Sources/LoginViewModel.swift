5766d27354d357a12f621a0890b8851d6eaa4383
surfing_seal <102800012+surfingseal@users.noreply.github.com>
2025-10-01 23:57:43 +0900

    Add files via upload

---
+++ megabox/Sources/LoginViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewModel.swift
+//  megabox
+//
+//  Created by 문인성 on 9/25/25.
+//
+
+import Foundation
+
+@Observable
+class LoginViewModel {
+    var loginInfo = LoginModel()
+    var isLoggedIn: Bool = false
+}
